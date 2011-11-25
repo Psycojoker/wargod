f66ce09f7d741b3989cb5b57a3df8c983571bd0c
Laurent Peuch <cortex@worlddomination.be>
2011-11-25 15:23:28 +0100

    [enh] parse rss file

--- wargod/wargod.py
+++ wargod/wargod.py
@@ -4,7 +4,14 @@ import sys
 import os
 #from config import config
 
+RSS_FILE = os.path.expanduser("~/.wargodrss")
+
 def run():
-    if not os.path.exists(os.path.expanduser("~/.wardogrss")):
-        sys.stderr.write("~/.wardogrss doesn't exist\nEnd\n")
+    if not os.path.exists(RSS_FILE):
+        sys.stderr.write("~/.wargodrss doesn't exist\nEnd\n")
         sys.exit(1)
+
+    print parse_feeds()
+
+def parse_feeds():
+    return [rss[:-1] for rss in open(RSS_FILE, "r")]
