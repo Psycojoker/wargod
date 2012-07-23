a2b4e8aadfc34684f53b2d7b15d443840f559a95
Laurent Peuch <cortex@worlddomination.be>
2012-07-23 11:40:04 +0200

    [enh] fill setup.py with informations

--- setup.py
+++ setup.py
@@ -6,20 +6,17 @@ from wargod import __version__
 
 setup(name='wargod',
       version=__version__,
-      description='FIXME',
-      author='FIXME',
-      long_description='FIXME',
-      # or you can also do something like this
-      # long_description=open("README").read(),
-      author_email='FIXME',
-      url='FIXME',
-      # install_requires=['lib_with_a_certain_version>=0.9.9.1', 'another_lib'],
-      # list of packages you want you application/lib install
+      description='river flow type minimaliste rss reader that generate static html pages ',
+      author='Laurent Peuch',
+      long_description=open("README").read(),
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/wargod',
+      install_requires=['beautifulsoup', 'lxml', 'feedparser', 'yaml', 'jinja2'],
       packages=['wargod'],
-      license= 'FIXME',
+      license= 'aGPLv3+',
       # list of scripts supplied by your application
       scripts=['bin/wargod'],
-      keywords='FIXME',
+      keywords='rss atom reader static html',
      )
 
 # vim:set shiftwidth=4 tabstop=4 expandtab:
